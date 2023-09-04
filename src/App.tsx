c16d9143a4087ba2d14b2bdf446c000dd89e707e
Muhammadrajab <muhammadr0jabxujaev@gamil.com>
2023-09-04 16:29:36 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { createElement as e, useState } from "react";
+import { Prodact } from "./conmponents/Prodact";
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto max-w-2xl">
+      <Prodact />
     </div>
   );
 }
---
+++ src/conmponents/Prodact.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Prodact() {
+  return <div className="font-bold mt-5">Prodact</div>;
+}
